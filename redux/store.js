731e6fdf88d588aa28f1b6a04c2423aa0f1c07ba
Clifford <kingsleyclifford2016@gmail.com>
2021-12-17 18:30:34 +0000

    added lines of code

--- redux/store.js
+++ redux/store.js
@@ -1,13 +1,16 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 import { reducers } from "./reducers";
+
+const middleware = [thunk];
+
 const initialState = {};
 
 const store = createStore(
-  reducers
-  // typeof window !== "undefined"
-  //   ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
-  //       window.__REDUX_DEVTOOLS_EXTENSION__()
-  //   : ""
+  reducers,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
